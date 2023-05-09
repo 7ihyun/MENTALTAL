fdb75bafd38ff97fe49a975017cecf0459d909f5
Kim-Jihyun1 <jh129047@gmail.com>
2023-05-09 23:06:35 +0900

    [feat] redis 연결 확인

---
+++ server/src/main/java/com/e1i5/mentaltal/auth/config/RedisExample.java
@@ -0,0 +1,24 @@
+package com.e1i5.mentaltal.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisExample {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    public RedisExample(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void setValue(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
