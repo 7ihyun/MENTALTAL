53c056238f624ec533426aeeb6f8b09de024c667
Kim-Jihyun1 <jh129047@gmail.com>
2023-06-26 20:39:36 +0900

    [fix] cors 수정

--- server/src/main/java/com/e1i5/mentaltal/config/WebConfig.java
+++ server/src/main/java/com/e1i5/mentaltal/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true) // 쿠키 요청 허용 (true 설정 시 보안상 이슈 발생 가능)
+                .allowCredentials(true) // 쿠키 요청 허용
                 .allowedOrigins("http://mentaltal-bucket.s3-website.ap-northeast-2.amazonaws.com")    // 공유를 허락할 origin
                 .allowedMethods("HEAD", "POST", "GET", "PATCH", "DELETE", "PUT") // 허용할 HTTP method 지정
                 .exposedHeaders("*");
